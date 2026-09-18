2cece728238d6e53d09aab8b5e161ab395b293b6
wtx0214 <2441515553@qq.com>
2026-09-18 16:20:50 +0800

    员工分页查询代码

--- sky-common/src/main/java/com/sky/result/PageResult.java
+++ sky-common/src/main/java/com/sky/result/PageResult.java
@@ -4,19 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.util.List;
 
-/**
- * 封装分页查询结果
- */
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PageResult implements Serializable {
-
-    private long total; //总记录数
-
-    private List records; //当前页数据集合
-
+@Data
+public class PageResult<T> {
+    private Long total;//总记录数
+    private List<T> rows;//当前页数据
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,9 +1,12 @@
 package com.sky.mapper;
 
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface EmployeeMapper {
@@ -19,4 +22,6 @@ public interface EmployeeMapper {
     @Insert("insert into employee (username, password, name, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "values (#{username}, #{password}, #{name}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
+
+    List<Employee> page(EmployeePageQueryDTO employeePageQueryDTO);
 }
